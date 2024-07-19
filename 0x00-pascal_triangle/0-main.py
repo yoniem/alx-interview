4b9aef31ae5217246f15f2f40a16b881d973e344
yoniem <yonimphini@gmail.com>
2024-07-19 07:19:20 -0700

    update

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -12,10 +12,23 @@ def print_triangle(triangle):
         print("[{}]".format(",".join([str(x) for x in row])))
 
 if __name__ == "__main__":
+    print("n = 5:")
     print_triangle(pascal_triangle(5))
     print()  # For better separation
+
+    print("n = 1:")
     print_triangle(pascal_triangle(1))
     print()  # For better separation
+
+    print("n = 0:")
     print_triangle(pascal_triangle(0))
     print()  # For better separation
+
+    print("n = 10:")
     print_triangle(pascal_triangle(10))
+    print()  # For better separation
+
+    # For large n, like 100, it's better to just check the last row
+    print("n = 100 (last row):")
+    triangle = pascal_triangle(100)
+    print("[{}]".format(",".join([str(x) for x in triangle[-1]])))
